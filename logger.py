[BUG-FIX | UPDATE] batching is achieved (gradient accumulation now not supported) (modified get_seq_batch inside wav2mov.py).Also basicConfig is used only base logger.logging level is configured to 'loggers' individually else where

logger.py
@@ -4,8 +4,9 @@ import re
 from datetime import datetime
 from torch.utils.tensorboard.writer import SummaryWriter
 # logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+logger.setLevel(logging.DEBUG)
 TIME_FORMAT = "%b %d,%Y %H:%M:%S"
 
 from pythonjsonlogger import jsonlogger

utils/plots.py
@@ -4,7 +4,8 @@ from torch.functional import Tensor
 import imageio
 import numpy as np
 from matplotlib import pyplot as plt
-
+import warnings
+warnings.filterwarnings( "ignore", module = "matplotlib\..*" )
 
 def show_img(img,cmap='viridis'):
     if isinstance(img,np.ndarray):