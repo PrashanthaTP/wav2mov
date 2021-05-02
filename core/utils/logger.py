2afaa7b313ca4befaef7cbef7a20d8c4c976b3c7
Prashantha T P <tpprashantha@gmail.com>
2021-05-02 12:40:26 +0530

    [TYPO-FIX | UPDATE] fixed typos in core.utils.logger and also FRACTION constant used during creating sub_batches inside wav2mov.py now taken from hparams

--- core/utils/logger.py
+++ core/utils/logger.py
@@ -1,10 +1,10 @@
 import logging
-logging.basicconfig(level=logging.error,format="%(levelname)s : %(name)s : %(asctime)s | %(msg)s ")     
+logging.basicConfig(level=logging.error,format="%(levelname)s : %(name)s : %(asctime)s | %(msg)s ")     
 
  
 def get_module_level_logger(name):
-    logger =  logging.getlogger(name)
-    logger.setlevel(logging.debug)
+    logger =  logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
     logger.propagate = False 
     return logger
  
