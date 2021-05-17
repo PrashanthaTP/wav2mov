69fcfaae9ea21de55868e9b9fb2f9542e92f2558
Prashantha T P <tpprashantha@gmail.com>
2021-05-17 22:41:26 +0530

    [UPDATE] test shell scripts updated

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+echo $1
 is_test='y'
-model_path='E:\Users\VS_Code_Workspace\Python\VirtualEnvironments\wav2mov\wav2mov\runs\v6\Run_7_4_2021__18_24\gen_Run_7_4_2021__18_24.pt'
+model_path='/content/drive/MyDrive/Colab Notebooks/projects/wav2mov_engine/wav2mov/runs/v10/'$1'/gen_'$1'.pt'
 log='n'
 device='cpu'
-version='v9'
+version='v10'
 
-python main/main.py --device=${device} --test=${is_test} --version=${version} --model_path=${model_path} --log=${log}
+python main/main.py --device=${device} --test=${is_test} --version=${version} --model_path="${model_path}" --log=${log} -v=14
