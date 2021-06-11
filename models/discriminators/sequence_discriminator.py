5c4062ca8d8bb3ae0b0e4e0dd949ddfad608dc5a
Prashantha T P <tpprashantha@gmail.com>
2021-06-11 22:53:24 +0530

    [TRAIN | CLEANUP] seq_disc : train on upper half of face

--- models/discriminators/sequence_discriminator.py
+++ models/discriminators/sequence_discriminator.py
@@ -1,8 +1,5 @@
-
 import torch
 from torch import nn,optim
-from torch.functional import Tensor
-
 
 from wav2mov.core.models.base_model import BaseModel
 from wav2mov.models.layers.conv_layers import Conv2dBlock
@@ -10,7 +7,6 @@ from wav2mov.models.utils import get_same_padding,squeeze_batch_frames
 
 from wav2mov.logger import get_module_level_logger
 logger = get_module_level_logger(__name__)
-
 
 class SequenceDiscriminator(BaseModel):
     def __init__(self,hparams):
@@ -22,8 +18,12 @@ class SequenceDiscriminator(BaseModel):
         chs = [in_channels] + self.hparams['chs']
         kernel,stride = 4,2
         padding = get_same_padding(kernel,stride)
-        cnn = nn.ModuleList([Conv2dBlock(chs[i],chs[i+1],kernel,stride,padding,use_norm=True,use_act=True,act=nn.LeakyReLU(0.2)) for i in range(len(chs)-2)])
-        cnn.append(Conv2dBlock(chs[-2],chs[-1],kernel,stride,padding,use_norm=False,use_act=True,act=nn.Tanh()))
+        cnn = nn.ModuleList([Conv2dBlock(chs[i],chs[i+1],kernel,stride,padding,
+                                         use_norm=True,use_act=True,
+                                         act=nn.LeakyReLU(0.01)) for i in range(len(chs)-2)])
+        cnn.append(Conv2dBlock(chs[-2],chs[-1],kernel,stride,padding,
+                               use_norm=False,use_act=True,
+                               act=nn.Tanh()))
         self.cnn = nn.Sequential(*cnn)
         ############################################
         # channels : 3  => 64 => 128 => 256 => 512          
@@ -32,16 +32,14 @@ class SequenceDiscriminator(BaseModel):
 
     def forward(self,frames):
         """frames : B,T,C,H,W"""
+        img_height = frames.shape[-2]
+        frames = frames[...,0:img_height//2,:]#consider upper half
         batch_size,num_frames,*img_size = frames.shape
         frames = squeeze_batch_frames(frames)
         frames = self.cnn(frames)
-        # logger.debug(frames.shape)
-    
         frames = frames.reshape(batch_size,num_frames,-1)
         out,_ = self.gru(frames)#out is of shape (batch_size,seq_len,num_dir*hidden_dim)
-        # logger.debug(out.shape) 
         return out[:,-1,:]#batch_size,hidden_size
 
     def get_optimizer(self):
         return optim.Adam(self.parameters(), lr=self.hparams['lr'], betas=(0.5,0.999))
-
