e112109133831bc999b28e5b9c915e0af78cf8de
Prashantha T P <tpprashantha@gmail.com>
2021-06-11 22:51:17 +0530

    [ARCH_CHANGE] (i) audio encoder : use residual connections (ii) add test for audio_encoder of gen

--- models/generator/audio_encoder.py
+++ models/generator/audio_encoder.py
@@ -1,35 +1,34 @@
 import torch
 from torch import nn
-
+from wav2mov.models.layers.conv_layers import Conv1dBlock
 from wav2mov.logger import get_module_level_logger
-from wav2mov.models.utils import squeeze_batch_frames
+from wav2mov.models.utils import squeeze_batch_frames,get_same_padding
 
 logger = get_module_level_logger(__name__)
 
 class AudioEnocoder(nn.Module):
-    """ 
-       >>> x = x.reshape(x.shape[0], 1, -1)
-       >>> x = self.conv(x)
-       >>> x = self.fc(x.reshape(x.shape[0],-1))
-       >>> return x #shape (batch_size,28*28)
-    """
-
     def __init__(self,hparams):
         super().__init__()
         self.hparams = hparams
-        use_bias = True
-        #audio = 666*5=3330
-        self.conv = nn.Sequential(
-            nn.Conv1d(1, 64, 330, 30,bias=use_bias),#input 666 output (3330-330+0)/30 + 1 = 101
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.Conv1d(64, 128, 3, 1,bias=use_bias),#((101-3+0)/1)+1 =98
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Conv1d(128, 1,4,2,1),#((98-4+2)/2)+1 = 48 +1 = 49
-            nn.Tanh(),
+        padding_31 = get_same_padding(3,1)
+        padding_32 = get_same_padding(3,2)
+        padding_42 = get_same_padding(4,2)
+        self.conv_encoder = nn.Sequential(
+            Conv1dBlock(1,64,330,30,padding=0,use_norm=True,use_act=True,act=nn.ReLU()),#101
+            
+            Conv1dBlock(64,128,3,1,padding=0,use_norm=True,use_act=True,act=nn.ReLU()),    #98
+            Conv1dBlock(128,128,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),    
+            Conv1dBlock(128,128,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),    
+            
+            Conv1dBlock(128,256,4,2,padding=padding_42,use_norm=True,use_act=True,act=nn.ReLU()),  #((98-4+2)/2 + 1) = 49
+            Conv1dBlock(256,256,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),    
+            Conv1dBlock(256,256,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),    
+           
+            Conv1dBlock(256,512,3,2,padding=padding_32,use_norm=True,use_act=True,act=nn.ReLU()),  # 49-3+2/2 + 1 = 25
+            Conv1dBlock(512,512,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),    
+            Conv1dBlock(512,512,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),    
             )
-        self.features_len = 49#out of conv layers
+        self.features_len = 25*512#out of conv layers
         self.num_layers = 1
         self.hidden_size = self.hparams['latent_dim_audio'] 
         self.gru = nn.GRU(input_size=self.features_len,
@@ -37,13 +36,11 @@ class AudioEnocoder(nn.Module):
                           num_layers=1,
                           batch_first=True)            
         self.final_act = nn.Tanh()
+        
     def forward(self, x):
-        # logger.debug(f'{x.shape}')
+        """ x : audio frames of shape B,T,666*5"""
         batch_size,num_frames,_ = x.shape
-        x = self.conv(squeeze_batch_frames(x).unsqueeze(1))
-        # x = self.audio_fc(x.reshape(x.shape[0], -1))
-        # logger.debug(f'audio after convs {x.shape}')
+        x = self.conv_encoder(squeeze_batch_frames(x).unsqueeze(1))
         x = x.reshape(batch_size,num_frames,self.features_len)
         x,_ = self.gru(x)
-        #B,seq_len,hidden_size
         return self.final_act(x)  # shape (batch_size,num_frames,hidden_size=latent_dim_audio)
