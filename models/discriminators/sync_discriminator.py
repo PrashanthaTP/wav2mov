e9f80b8bbf6a9f97a86e252b26dc7b432ac931d2
Prashantha T P <tpprashantha@gmail.com>
2021-06-11 22:50:10 +0530

    [ARCH_CHANGE] (i) use residual connections in sync disc (ii) add test for sync_disc

--- models/discriminators/sync_discriminator.py
+++ models/discriminators/sync_discriminator.py
@@ -2,6 +2,10 @@ import torch
 from torch import nn, optim
 
 
+from wav2mov.models.layers.conv_layers import Conv1dBlock,Conv2dBlock
+from wav2mov.models.layers.debug_layers import IdentityDebugLayer
+from wav2mov.models.utils import get_same_padding
+
 from wav2mov.logger import get_module_level_logger
 logger = get_module_level_logger(__name__)
 
@@ -9,89 +13,98 @@ from wav2mov.core.models.base_model import BaseModel
 from wav2mov.models.discriminators.utils import save_series
 
 i= 0
-import matplotlib.pyplot as plt
-import os
 
 class SyncDiscriminator(BaseModel):
-    def __init__(self, hparams,config,use_bias=False):
+    def __init__(self, hparams,config):
         super().__init__()
         self.hparams = hparams 
         self.config = config
-        #5 frames of 666 :3330
         self.relu_neg_slope = self.hparams['relu_neg_slope'] 
-        self.disc_a = nn.Sequential(
-            nn.Conv1d(1, 64, 494, 50,bias=use_bias),#input 5994 output (5994-494+0)/50 + 1 = 111
-            nn.BatchNorm1d(64),
-            nn.LeakyReLU(self.relu_neg_slope),
-            nn.Conv1d(64, 128, 4, 1,bias=use_bias),#((111-4+0)/1)+1 =108
-            nn.BatchNorm1d(128),
-            nn.LeakyReLU(self.relu_neg_slope),
-            nn.Conv1d(128, 256,4,2,1),#((108-4+2)/2)+1 = 53 +1 = 54
-            nn.BatchNorm1d(256),
-            nn.LeakyReLU(self.relu_neg_slope),
-            nn.Conv1d(256, 256,4,2),#((54-4)/2)+1 = 25 + 1 = 26
-            nn.LeakyReLU(self.relu_neg_slope),
-            )
-        self.audio_fc = nn.Sequential(nn.Linear(26*256,5*256),
-                                      nn.LeakyReLU(self.relu_neg_slope),
-                                      nn.Linear(5*256,256),
-                                      nn.LeakyReLU(self.relu_neg_slope),
-                                      )
-        # height and width are halved in each step
-        self.pre_disc_v = nn.Sequential(
-            nn.Conv3d(self.hparams['in_channels'], 32, (5,4,4), (1,2,2),(0,1,1), bias=use_bias),
+        ##############################################################
+        # Audio encoding
+        #5 frames of 666 :3330 + 666*4 = 5994
+        # size     : 3330-> 
+        # k,s,p          ->(666,50) 
+        # channels :   1 -> 64
+        ################################################################
+        # 1->64->128->256->512
+        padding_42 = get_same_padding(kernel_size=4,stride=2)#(4-2)/2 = 1
+        padding_31 = get_same_padding(kernel_size=3,stride=1)#(3-1)/2 = 1
+        padding_32 = get_same_padding(kernel_size=3,stride=2)#(3-2)/2 = 1
+        self.audio_encoder = nn.Sequential(
+            Conv1dBlock(1,32,666,1,0,use_norm=True,use_act=True,act=nn.ReLU()),#5994=>5328,
+            
+            Conv1dBlock(32,64,4,2,padding=padding_42,use_norm=True,use_act=True,act=nn.ReLU()),#5328=>2664,
+            Conv1dBlock(64,64,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            Conv1dBlock(64,64,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            
+            Conv1dBlock(64,128,4,2,padding=padding_42,use_norm=True,use_act=True,act=nn.ReLU()),#2664=>1332
+            Conv1dBlock(128,128,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            Conv1dBlock(128,128,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            
+            Conv1dBlock(128,256,3,2,padding=padding_42,use_norm=True,use_act=True,act=nn.ReLU()),#1332=>666
+            Conv1dBlock(256,256,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            Conv1dBlock(256,256,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            
+            Conv1dBlock(256,512,4,2,padding=padding_42,use_norm=True,use_act=True,act=nn.ReLU()),#666=>333
+            Conv1dBlock(512,512,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            Conv1dBlock(512,512,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+        
+            Conv1dBlock(512,512,3,2,padding=padding_32,use_norm=True,use_act=True,act=nn.ReLU()),#((333-3+2)/2)+1=167
+            Conv1dBlock(512,512,3,2,padding=padding_32,use_norm=True,use_act=True,act=nn.ReLU()),#((167-3+2)/2)+1=84
+            Conv1dBlock(512,512,4,2,padding=padding_42,use_norm=True,use_act=True,act=nn.ReLU()),#((84-4+2)/2)+1=42
+            Conv1dBlock(512,512,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU()),
+            
+            ) 
+        self.audio_fc = nn.Sequential(
+            nn.Linear(42*512,512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Linear(512,256),
+            nn.ReLU()
+            ) 
+        # 3=>3d cnn => 32 ->64->128->256->512
+        self.frames_3d_to_2d = nn.Sequential(
+            nn.Conv3d(self.hparams['in_channels'], 32, (5,4,4), (1,2,2),(0,1,1) ),
             nn.BatchNorm3d(32),
-            nn.LeakyReLU(self.relu_neg_slope))
-
-        ##############################################
-        #pre_disc_V : F :5 ==> ((5-5)+0)/1) + 1 =>1 
-        #            H : 128 ==>((128-4+2)/2) + 1 =>64
-        #           W : 256 ==>((256-4+2)/2) + 1 =>128
-        # out is of shape : 32x64x128
-        #############################################
-        self.disc_v = nn.Sequential( 
-            nn.Conv2d(32, 64, 4, 2, 1,bias=use_bias), # #H_new = 32, W_new = 64     
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(self.relu_neg_slope),  
-            nn.Conv2d(64, 128, 4, 2, 1,bias=use_bias), #H_new,W_new = 16,32
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(self.relu_neg_slope),
-            nn.Conv2d(128, 256, 4, 2, 1),  #H_new,W_new = 8,16
-            nn.LeakyReLU(self.relu_neg_slope)
-
-        )
+            nn.ReLU(),
+            # height and width are halved in each step
+            ##############################################
+            #pre_disc_V : F :5 ==> ((5-5)+0)/1) + 1 =>1 
+            #            H : 128 ==>((128-4+2)/2) + 1 =>64
+            #           W : 256 ==>((256-4+2)/2) + 1 =>128
+            # out is of shape : 32x64x128
+            #############################################
+            ) 
+        self.frames_encoder = nn.Sequential(
+            Conv2dBlock(32,64,4,2,padding_42,use_norm=True,use_act=True,act=nn.ReLU()),#H:64=>32 and W:128=>64
+            Conv2dBlock(64,64,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            Conv2dBlock(64,64,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            
+            Conv2dBlock(64,128,4,2,padding=padding_42,use_norm=True,use_act=True,act=nn.ReLU()),#16,32
+            Conv2dBlock(128,128,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            Conv2dBlock(128,128,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            
+            Conv2dBlock(128,256,4,2,padding=padding_42,use_norm=True,use_act=True,act=nn.ReLU()),#8,16
+            Conv2dBlock(256,256,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            Conv2dBlock(256,256,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            
+            Conv2dBlock(256,512,4,2,padding=padding_42,use_norm=True,use_act=True,act=nn.ReLU()),#4,8
+            Conv2dBlock(512,512,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
+            Conv2dBlock(512,512,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU(),residual=True),
         
-        self.video_fc = nn.Sequential(nn.Linear(256*8*16,256*8),
-                                      nn.BatchNorm1d(256*8),
-                                      nn.LeakyReLU(self.relu_neg_slope),
-                                      nn.Linear(256*8,256),
-                                      nn.LeakyReLU(self.relu_neg_slope))
-        # self.fc = nn.Sequential(
-        #     nn.Linear(256+256, 256),
-        #     nn.ReLU(),
-        #     nn.Linear(256, 128)
-        #     )
-        # self.slp = nn.Sequential(nn.Linear(1,1))
-        # self.slp = nn.Sequential(nn.Linear(1,128),
-        #                         nn.ReLU(),
-        #                         nn.Linear(128,1))
-        # self.slp = nn.Sequential(nn.Linear(256*2,128),
-        #                         nn.ReLU(),
-        #                         nn.Linear(128,1))
+            Conv2dBlock(512,512,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU()),#4,8
+            Conv2dBlock(512,512,3,1,padding=padding_31,use_norm=True,use_act=True,act=nn.ReLU()),
+            )        
+        self.frames_fc = nn.Sequential(nn.Linear(4*8*512,512),
+                                      nn.BatchNorm1d(512),
+                                      nn.ReLU(),
+                                      nn.Linear(512,256),
+                                      nn.ReLU())
 
-    def save_image(self,image):
-        global i
-        if i>2:return
-        image = image.permute(0,2,3,4,1)[0][0].squeeze().cpu()#B,F,H,W,C
-        image = image.numpy().astype('float32')
-        save_path = os.path.join(self.config['runs_dir'],'images',f'sync_lower_half_{i}.png')
-        os.makedirs(os.path.dirname(save_path),exist_ok=True)
-        i+=1
-        plt.imshow(image,cmap='gray')
-        plt.savefig(save_path)
-        logger.debug(f'[SYNC] image saved in {save_path}')
         
-
+    def swap_channel_frame_axes(self,video):
+        return video.permute(0,2,1,3,4)#B,F,C,H,W to B,C,F,H,W
 
     def forward(self, audio_frames, video_frames):
         global i
@@ -100,43 +113,29 @@ class SyncDiscriminator(BaseModel):
         """
 
         batch_size = audio_frames.shape[0]
-        # logger.debug(f'inside sync forward : audio : {audio_frames.shape} {audio_frames.is_cuda} : video {video_frames.shape} {video_frames.is_cuda}') 
-        
         img_height = video_frames.shape[-2]
         video_frames = video_frames[...,img_height//2:,:] #consider only lower half of the image so new height is 256/2 = 128
 
+        # if i<4:
+        #   save_series(video_frames[0].unsqueeze(1),self.config,i)
+        #   i+=1
 
-        if i<4:
-          save_series(video_frames[0].permute(1,0,2,3).unsqueeze(1),self.config,i)
-          i+=1
-        # self.save_image(video_frames)
-
-        video_embeddings = self.pre_disc_v(video_frames)#out has no Temporal(i,e where frames are stacked) dimension | 3d (F,H,W) to 2d(H,W)
-        video_embeddings = video_embeddings.reshape(batch_size,
-                                                    video_embeddings.shape[1],
-                                                    video_embeddings.shape[-2],
-                                                    video_embeddings.shape[-1])
-                                                    
-        video_embeddings = self.disc_v(video_embeddings).reshape(batch_size, -1)
-        video_embeddings = self.video_fc(video_embeddings)
+        video_frames = self.swap_channel_frame_axes(video_frames)    
+        video_embeddings = self.frames_3d_to_2d(video_frames)#out has no Temporal(i,e where frames are stacked) dimension | 3d (F,H,W) to 2d(H,W)
+        video_embeddings = torch.squeeze(video_embeddings,dim=2)#B,C,F=1,H,W ==> B,C,H,W
+        video_embeddings = self.frames_encoder(video_embeddings).reshape(batch_size, -1)
+        video_embeddings = self.frames_fc(video_embeddings)
         
         audio_frames = audio_frames.reshape(batch_size, 1, -1)
-    
-        # print(f'audio frames {audio_frames.shape} self.disc_a(audio_frames) {self.disc_a(audio_frames).shape}')
-
-        audio_embeddings = self.disc_a(audio_frames).reshape(batch_size,-1)
+        audio_embeddings = self.audio_encoder(audio_frames).reshape(batch_size,-1)
         audio_embeddings = self.audio_fc(audio_embeddings)
-        # print(f'audio frames {audio_frames.shape}') 
         #see syncnet forward pass https://github.com/Rudrabha/Wav2Lip/blob/master/models/syncnet.py#L62-L63
         audio_embeddings = nn.functional.normalize(audio_embeddings,p=2,dim=1) 
         video_embeddings = nn.functional.normalize(video_embeddings,p=2,dim=1) 
         
         cosine_dist = nn.functional.cosine_similarity(audio_embeddings,video_embeddings)
         cosine_dist = cosine_dist.reshape(batch_size,-1)
-        classification = cosine_dist
-        # classification = self.slp(cosine_dist)
-        # classification = self.slp(torch.cat([audio_embeddings,video_embeddings],dim=1)) #concat to get (B,size_audio_embedding+size_video_embedding)
-        return (classification,)
+        return (cosine_dist,)
 
     def get_optimizer(self):
         return optim.Adam(self.parameters(), lr=self.hparams['lr'], betas=(0.5,0.999))
