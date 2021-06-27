ffe48a487cf13b9e246b09c0a4c63767008b17f4
Prashantha T P <tpprashantha@gmail.com>
2021-06-27 22:59:07 +0530

    [UPDATE] set version before model path

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+VERSION="v15_sync_expert"
 echo $1
 is_test='y'
-model_path='/content/drive/MyDrive/Colab Notebooks/projects/wav2mov_engine/wav2mov/runs/v10/'$1'/gen_'$1'.pt'
+model_path="/content/drive/MyDrive/Colab Notebooks/wav2mov-dev_phase_6/wav2mov/runs/${VERSION}/${1}/gen_${1}.pt"
 log='n'
 device='cpu'
-version='v10'
 
-python main/main.py --device=${device} --test=${is_test} --version=${version} --model_path="${model_path}" --log=${log} -v=14
+python main/main.py --device=${device} --test=${is_test} --version=${VERSION} --model_path="${model_path}" --log=${log} -v=14
