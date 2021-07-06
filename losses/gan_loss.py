32530929293d0dc736fc88e86ab4fe7876fe6b25
Prashantha T P <tpprashantha@gmail.com>
2021-07-06 14:51:04 +0530

    [CRITERION] use random real lable for every pass

--- losses/gan_loss.py
+++ losses/gan_loss.py
@@ -5,20 +5,19 @@ from torch import nn
 class GANLoss(nn.Module):
     """ To abstract away the task of creating real/fake labels and calculating loss
     [Reference]: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
-    
-    """
-    
+    """    
     def __init__(self,device,real_label=None,fake_label=0.0):
         super().__init__()
-        if real_label is None:
-            real_label = round(random.uniform(0.8,1),2)
-        self.register_buffer('real_label',torch.tensor(real_label))
-        self.register_buffer('fake_label',torch.tensor(fake_label))
+        self.real_label = real_label
+        self.fake_label = fake_label
+        # self.register_buffer('real_label',torch.tensor(real_label))
+        # self.register_buffer('fake_label',torch.tensor(fake_label))
         self.loss = nn.BCEWithLogitsLoss()
         self.device = device 
         
     def get_target_tensor(self,preds,is_real_target):
-        target_tensor = self.real_label if is_real_target else self.fake_label
+        real_label = torch.tensor( round(random.uniform(0.8,1),2) if self.real_label is None else self.real_label) 
+        target_tensor  = real_label if is_real_target else torch.tensor(self.fake_label)
         return target_tensor.expand_as(preds).to(self.device)
     
     def forward(self,preds,is_real_target):
--- losses/sync_loss.py
+++ losses/sync_loss.py
@@ -12,24 +12,21 @@ class SyncLoss(nn.Module):
     """
     def __init__(self,device,real_label=None,fake_label=0.0):
         super().__init__()
-        if real_label is None:
-            real_label = round(random.uniform(0.8,1),2)
-        self.register_buffer('real_label',torch.tensor(real_label))
-        self.register_buffer('fake_label',torch.tensor(fake_label))
+        self.real_label = real_label
+        self.fake_label = fake_label
+        # self.register_buffer('real_label',torch.tensor(real_label))
+        # self.register_buffer('fake_label',torch.tensor(fake_label))
         self.loss = nn.BCELoss()
-        #why not BCELossWithLogits ? (which has sigmoid layer at the entrance)
-        # Its because loss is being calculated with respect to cosine distance which is already in the range(0,1)
-        # Also using bceloss puts larger penalty while the one having inbuilt sigmoid results in smoother loss
         self.device = device
         
     def get_target_tensor(self,preds,is_real_target):
-        target_tensor = self.real_label if is_real_target else self.fake_label
+        real_label = torch.tensor( round(random.uniform(0.8,1),2) if self.real_label is None else self.real_label )
+        target_tensor =  if is_real_target else torch.tensor(self.fake_label)
         return target_tensor.expand_as(preds).to(self.device)
     
     def forward(self,preds,is_real_target):
         # preds = nn.functional.cosine_similarity(audio_embedding,video_embedding)
         target_tensor = self.get_target_tensor(preds,is_real_target)
         loss = self.loss(preds,target_tensor)
-        logger.debug(f'[sync] loss : {is_real_target} frames  | {loss.item():0.4f}')
+        logger.debug(f'[sync] loss {is_real_target} frames :{loss.item():0.4f}')
         return loss
-    
