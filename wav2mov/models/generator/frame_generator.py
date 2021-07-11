00e098f3a7e6129f2819474fb918119a65418155
Prashantha T P <tpprashantha@gmail.com>
2021-07-11 21:08:03 +0530

    [TRAIN] remove noise encoder

--- wav2mov/models/generator/frame_generator.py
+++ wav2mov/models/generator/frame_generator.py
@@ -17,17 +17,17 @@ class Generator(nn.Module):
         self.id_encoder = IdEncoder(self.hparams)
         self.id_decoder = IdDecoder(self.hparams)
         self.audio_encoder = AudioEnocoder(self.hparams)
-        self.noise_encoder = NoiseEncoder(self.hparams)
+        # self.noise_encoder = NoiseEncoder(self.hparams)
 
     def forward(self,audio_frames,ref_frames):
         batch_size,num_frames,*_ = ref_frames.shape
         assert num_frames == audio_frames.shape[1]
-        encoded_id , intermediates = self.id_encoder(squeeze_batch_frames(ref_frames))#encoded id is from 1024=>1 
+        encoded_id , intermediates = self.id_encoder(squeeze_batch_frames(ref_frames))
         encoded_id = encoded_id.reshape(batch_size*num_frames,-1,1,1)
         encoded_audio = self.audio_encoder(audio_frames).reshape(batch_size*num_frames,-1,1,1)
-        encoded_noise = self.noise_encoder(batch_size,num_frames).reshape(batch_size*num_frames,-1,1,1)
+        # encoded_noise = self.noise_encoder(batch_size,num_frames).reshape(batch_size*num_frames,-1,1,1)
         # logger.debug(f'encoded_id {encoded_id.shape} encoded_audio {encoded_audio.shape} encoded_noise {encoded_noise.shape}')
-        encoded = torch.cat([encoded_id,encoded_audio,encoded_noise],dim=1)#along channel dimension
+        encoded = torch.cat([encoded_id,encoded_audio],dim=1)#along channel dimension
         gen_frames =  self.id_decoder(encoded,intermediates)
         _,*img_shape = gen_frames.shape
         return gen_frames.reshape(batch_size,num_frames,*img_shape)
